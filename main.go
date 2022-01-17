5557591d876278d9361fa9e0885f611de946ce65
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-01-17 11:05:37 +0100

    Add main function

--- main.go
+++ main.go
@@ -9,4 +9,50 @@
 //
 //********************************************************************************************************************//
 
-package goten_cli
+package main
+
+import (
+	"fmt"
+	"github.com/gowizzard/goten"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	// Check arguments
+	if len(os.Args) == 2 {
+
+		// Format to number
+		length, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+
+			// Log error
+			fmt.Println("Sorry your argument is not a number.")
+
+			// Exit
+			os.Exit(1)
+
+		}
+
+		// Generate the password
+		password := goten.New(length)
+
+		// Print the password
+		fmt.Println("Here is your password:")
+		fmt.Println(password)
+
+		// Exit
+		os.Exit(0)
+
+	} else {
+
+		// Log error
+		fmt.Println("Usage: goten <length>")
+
+		// Exit with error code
+		os.Exit(1)
+
+	}
+
+}
